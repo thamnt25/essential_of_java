cda16948bed6d717bbd279e96940978771700083
thamnt25 <thamnt258@gmail.com>
2023-01-19 10:50:07 -0500

    change java verion 11 - 8

--- src/main/java/org/example/Calculator.java
+++
@@ -1,5 +0,0 @@
-package org.example;
-
-public interface Calculator {
-    public int calculate(int x, int y);
-}
---
+++ src/main/java/org/example/isbntools/StockManager.java
@@ -0,0 +1,15 @@
+package org.example.isbntools;
+import org.example.isbntools.ExternalISBNDataService;
+
+public class StockManager {
+
+    public ExternalISBNDataService service;
+
+    public void setExternalISBNDataService(ExternalISBNDataService service) {
+        this.service = service;
+    }
+
+    public String getLocatorCode(String inbs) {
+        Book book =
+    }
+}
